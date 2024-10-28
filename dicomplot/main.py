545728b49e80b881a0711e77120394e644a2cd47
Niels Bassler <bassler@clin.au.dk>
2024-10-28 14:23:32 +0100

    fix #1

--- dicomplot/main.py
+++ dicomplot/main.py
@@ -6,7 +6,8 @@ import matplotlib.pyplot as plt
 from matplotlib.widgets import Button
 import logging
 
-import matplotlib.ticker as ticker  # Import ticker module for setting tick locators
+# Import ticker module for setting tick locators
+import matplotlib.ticker as ticker
 
 
 logger = logging.getLogger(__name__)
@@ -16,7 +17,8 @@ def get_spot_map(dicom_object, ion_beam_sequence_index=0, ion_control_point_sequ
     """Get the spot map from the dicom object.
 
     """
-    logger.info(f"Getting spot map from dicom object {ion_beam_sequence_index}, {ion_control_point_sequence_index}")
+    logger.info(
+        f"Getting spot map from dicom object {ion_beam_sequence_index}, {ion_control_point_sequence_index}")
     ibs = dicom_object.IonBeamSequence[ion_beam_sequence_index]
     cps = ibs.IonControlPointSequence[ion_control_point_sequence_index]
 
@@ -37,12 +39,18 @@ def plot_map(field_index, energy_layer_index, maps, ax, cbar, fig, max_weight):
     # Normalize spot sizes: This might need adjustment depending on the desired appearance
     sizes = np.array(weights) * 100 / max_weight  # Example scaling factor
 
-    scatter = ax.scatter(x, y, c=weights, cmap='cividis', s=sizes, vmin=0, vmax=max_weight)
+    scatter = ax.scatter(x, y, c=weights, cmap='nipy_spectral', s=sizes,
+                         vmin=0, vmax=max_weight, edgecolors='black', linewidths=0.5, alpha=0.7)
+
+    ax.set_aspect('equal')
 
     # Update the color bar
     if cbar:
-        cbar.remove()
-    cbar = fig.colorbar(scatter, ax=ax, label='Spot Weight', orientation='vertical')
+        cbar.update_normal(scatter)
+    else:
+        # Using cividis colormap for better colorblind accessibility
+        cbar = fig.colorbar(scatter, ax=ax, label='Spot Weight [MU]',
+                            orientation='vertical')
 
     # Set major grid and ticks for 1 cm spacing
     ax.set_xticks(np.arange(np.floor(min(x)), np.ceil(max(x)) + 1, 1))
@@ -56,12 +64,12 @@ def plot_map(field_index, energy_layer_index, maps, ax, cbar, fig, max_weight):
     ax.grid(True, which='major', color='gray', linestyle='--', linewidth=0.5)
     ax.grid(True, which='minor', color='gray', linestyle=':', linewidth=0.5)
 
-    ax.set_title(f"Field {field_index + 1}, Energy Layer {energy_layer_index + 1} - Beam Energy: {energy if energy != -1 else 'N/A'} MeV")
+    ax.set_title(
+        f"Field {field_index + 1}, Energy Layer {energy_layer_index + 1} - Beam Energy: {energy if energy != -1 else 'N/A'} MeV")
     ax.set_xlabel('X Position [cm]')
     ax.set_ylabel('Y Position [cm]')
     plt.draw()
     return cbar
-
 
 
 def create_interactive_plot(maps, max_weight):
@@ -72,31 +80,38 @@ def create_interactive_plot(maps, max_weight):
     cbar = None  # Initial color bar is None
 
     # Initial plot and create initial color bar
-    cbar = plot_map(field_index[0], energy_layer_index[0], maps, ax, cbar, fig, max_weight)
+    cbar = plot_map(field_index[0], energy_layer_index[0],
+                    maps, ax, cbar, fig, max_weight)
 
     # Define button click events, including passing max_weight
     def next_field(event):
         nonlocal cbar
         field_index[0] = (field_index[0] + 1) % len(maps)
-        energy_layer_index[0] = 0  # Reset energy layer index when changing fields
-        cbar = plot_map(field_index[0], energy_layer_index[0], maps, ax, cbar, fig, max_weight)
+        # Reset energy layer index when changing fields
+        energy_layer_index[0] = 0
+        cbar = plot_map(
+            field_index[0], energy_layer_index[0], maps, ax, cbar, fig, max_weight)
 
     def prev_field(event):
         nonlocal cbar
         field_index[0] = (field_index[0] - 1) % len(maps)
         energy_layer_index[0] = 0
-        cbar = plot_map(field_index[0], energy_layer_index[0], maps, ax, cbar, fig, max_weight)
-
+        cbar = plot_map(
+            field_index[0], energy_layer_index[0], maps, ax, cbar, fig, max_weight)
 
     def next_layer(event):
         nonlocal cbar
-        energy_layer_index[0] = (energy_layer_index[0] + 1) % len(maps[field_index[0]])
-        cbar = plot_map(field_index[0], energy_layer_index[0], maps, ax, cbar, fig, max_weight)
+        energy_layer_index[0] = (
+            energy_layer_index[0] + 1) % len(maps[field_index[0]])
+        cbar = plot_map(
+            field_index[0], energy_layer_index[0], maps, ax, cbar, fig, max_weight)
 
     def prev_layer(event):
         nonlocal cbar
-        energy_layer_index[0] = (energy_layer_index[0] - 1) % len(maps[field_index[0]])
-        cbar = plot_map(field_index[0], energy_layer_index[0], maps, ax, cbar, fig, max_weight)
+        energy_layer_index[0] = (
+            energy_layer_index[0] - 1) % len(maps[field_index[0]])
+        cbar = plot_map(
+            field_index[0], energy_layer_index[0], maps, ax, cbar, fig, max_weight)
 
     # Button placement and creation
     ax_field_prev = plt.axes([0.1, 0.05, 0.1, 0.075])
@@ -116,6 +131,7 @@ def create_interactive_plot(maps, max_weight):
 
     plt.show()
 
+
 def find_global_max_weight(maps):
     """Find the maximum weight across all maps."""
     max_weight = 0
@@ -125,21 +141,22 @@ def find_global_max_weight(maps):
     return max_weight
 
 
-
 def main(args=None):
     """Main routine with option parsing."""
     if args is None:
         args = sys.argv[1:]
 
-    parser = argparse.ArgumentParser(description='Plot DICOM proton therapy treatment plans.')
+    parser = argparse.ArgumentParser(
+        description='Plot DICOM proton therapy treatment plans.')
     parser.add_argument('inputfile', help='input filename', type=str)
     parser.add_argument('-v', '--verbosity', action='count', default=0,
                         help='Increase output verbosity.')
     parsed_args = parser.parse_args(args)
 
-    logging.basicConfig(level=logging.INFO if parsed_args.verbosity else logging.WARNING)
+    logging.basicConfig(
+        level=logging.INFO if parsed_args.verbosity else logging.WARNING)
 
-    dicom_object = pydicom.read_file(parsed_args.inputfile)
+    dicom_object = pydicom.dcmread(parsed_args.inputfile)
 
     maps = []
     for ibs_index in range(len(dicom_object.IonBeamSequence)):
@@ -153,6 +170,6 @@ def main(args=None):
     max_weight = find_global_max_weight(maps)
     create_interactive_plot(maps, max_weight)
 
+
 if __name__ == '__main__':
     sys.exit(main(sys.argv[1:]))
-
